d4fb0958c6d9dd544ea71b0ddadef11d136710b9
emoqbao <emoqbao@github.com>
2026-05-24 00:48:41 +0800

    Fix checkmark: tips thin, corner thick, larger size

--- src/components/ui/CheckIcon.tsx
+++ src/components/ui/CheckIcon.tsx
@@ -1,7 +1,7 @@
 export default function CheckIcon() {
   return (
-    <svg width="10" height="10" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M13.2 3.8L6.5 10.8L3.2 7.2L2.2 8.1L6.5 12.2L14.2 4.7Z" fill="#7c5ce7" />
+    <svg width="12" height="12" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M14.5 4L6.5 11.5L3 7.5L2.3 8.2L6.5 13.5L15.2 4.7Z" fill="#7c5ce7" />
     </svg>
   );
 }
