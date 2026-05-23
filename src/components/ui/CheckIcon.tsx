a1e6a7ebd2fd22eee55eaa0dcd39a075a836e643
emoqbao <emoqbao@github.com>
2026-05-24 00:52:24 +0800

    Calligraphic checkmark: 4.7x thickness ratio, larger size

--- src/components/ui/CheckIcon.tsx
+++ src/components/ui/CheckIcon.tsx
@@ -1,7 +1,7 @@
 export default function CheckIcon() {
   return (
-    <svg width="12" height="12" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M14.5 4L6.5 11.5L3 7.5L2.3 8.2L6.5 13.5L15.2 4.7Z" fill="#7c5ce7" />
+    <svg width="14" height="14" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M16 3.5L7 12L3 7.5L2.5 8L8.5 15L16.5 3Z" fill="#7c5ce7" />
     </svg>
   );
 }
